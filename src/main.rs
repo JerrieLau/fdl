776500257afc0b3214511a7f9bf2d9b5cbfbea9b
liujie <liujie@ebnew.com>
2017-03-28 23:51:32 +0800

    调整代码

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,11 @@ use std::process;
 use regex::Regex;
 use mods::config::Config;
 
-
 fn main() {
     //1.解析输入参数
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("解析输入参数出错，详情：{}", e);
+        println!("解析输入参数出错，详情：{}!", e);
         process::exit(1);
     });
     let url = config.url;
@@ -27,26 +26,34 @@ fn main() {
     let filename = config.filename;
 
     //2. 获取文件分片信息
-    let mut res = reqwest::get(url.as_str()).unwrap();
-    if !res.status().is_success() {
-        for i in 0..config.retry {
-            res = reqwest::get(url.as_str()).unwrap();
-            if !res.status().is_success() {
+    let mut content = Vec::<u8>::new();
+    for i in 0..config.retry {
+        let result = reqwest::get(url.as_str());
+        if !result.is_ok() {
+            println!("获取下载文件分片失败，重试次数{}次!", i + 1);
+            if i == config.retry - 1 {
+                process::exit(1);
+            } else {
+                continue;
+            }
+        } else {
+            let mut res = result.unwrap();
+            if ! res.status().is_success() {
+                println!("获取下载文件分片失败，重试第{}次!", i + 1);
                 if i == config.retry - 1 {
-                    println!("获取下载文件分片信息出错，状态码：{}", res.status());
+                    println!("获取下载文件分片数据时重试{}次仍失败，退出!", config.retry);
                     process::exit(1);
                 } else {
-                    println!("获取下载文件分片信息出错{}次", i + 1);
                     continue;
                 }
-            } else {
-                break;
             }
+            io::copy(&mut res, &mut content).unwrap();
+            println!("获取下载文件分片信息完成!");
+            break;
         }
     }
 
-    let mut content = Vec::<u8>::new();
-    io::copy(&mut res, &mut content).unwrap();
+
     let re = Regex::new(r"#\w+").unwrap();
     let reader = BufReader::new(content.as_slice());
     let mut segments = vec!();
@@ -79,22 +86,33 @@ fn main() {
         segment_url.push_str(segment.as_str());
 
         for i in 0..config.retry {
-            let mut res = reqwest::get(segment_url.as_str()).unwrap();
-            if !res.status().is_success() {
+            let result = reqwest::get(segment_url.as_str());
+            if ! result.is_ok() {
+                println!("下载分片({})出错，重试第{}次!", segment, i + 1);
                 if i == config.retry - 1 {
-                    println!("下载分片({})的数据时出错，状态码：{}", segment, res.status());
+                    println!("下载分片({})的数据时重试{}次仍失败，退出!", segment, config.retry);
                     process::exit(1);
                 } else {
-                    println!("下载分片({})出错{}次", segment, i + 1);
                     continue;
                 }
+            } else {
+                let mut res = result.unwrap();
+                //http status is fail
+                if !res.status().is_success() {
+                    println!("下载分片({})出错，重试第{}次!", segment, i + 1);
+                    if i == config.retry - 1 {
+                        println!("下载分片({})的数据时重试{}次仍失败，状态码:{}，退出!", segment, config.retry, res.status());
+                        process::exit(1);
+                    } else {
+                        continue;
+                    }
+                } else {
+                    //4. 保存文件
+                    io::copy(&mut res, &mut file).unwrap();
+                    println!("文件({})的分片({})下载完成!", filename, segment);
+                    break;
+                }
             }
-            //4. 保存文件
-            io::copy(&mut res, &mut file).unwrap();
-            println!("文件({})的分片({})下载完成，", filename, segment);
-
-            break;
         }
     }
-    //    println!("content is {}", );
 }
